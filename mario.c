30d5ffe8c38d74edfd0f241fcada83273433b62d
qichaozhao <qichaozhao@users.noreply.github.com>
2019-01-17 13:41:54 +0000

    20190117T134112Z

---
+++ mario.c
@@ -0,0 +1,60 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void pyramid_left(int height, int row);
+void pyramid_right(int height, int row);
+
+int main(void)
+{
+	int h;
+	do
+	{
+		h = get_int("Height: ");
+	}
+	while (h < 1 || h > 8);
+
+	for (int r = 1; r <= h; r++)
+    {
+        // Print left part
+        pyramid_left(h, r);
+       	
+       	// Print two spaces
+       	printf("  ");
+
+        // Print right part
+        pyramid_right(h, r);
+        
+        // Print newline
+        printf("\n");
+    }
+}
+
+void pyramid_left(int height, int row)
+{
+    // Print spaces
+    for (int r = 1; r <= (height - row); r++)
+    {
+        printf(" ");
+    }
+    
+    // Print blocks
+    for (int r = 1; r <= row; r++)
+    {
+        printf("#");
+    }
+}
+
+void pyramid_right(int height, int row)
+{
+    // Print blocks
+    for (int r = 1; r <= row; r++)
+    {
+        printf("#");
+    }
+    
+    // Print spaces
+    for (int r = 1; r <= (height - row); r++)
+    {
+        printf(" ");
+    }
+}
