84a01d5fe35557277d9a6291ffba76e10a0034e8
Qichao Zhao <qczhao@gmail.com>
2019-02-13 22:44:01 +0800

    first commit

---
+++ helpers.py
@@ -0,0 +1,25 @@
+def lines(a, b):
+    """Return lines in both a and b"""
+
+    b_lines = b.split("\n")
+    return [l for l in a.split("\n") if l in b_lines]
+
+
+def sentences(a, b):
+    """Return sentences in both a and b"""
+
+    from nltk.tokenize import sent_tokenize
+
+    b_sents = sent_tokenize(b)
+
+    return set([w for w in sent_tokenize(a) if w in b_sents])
+
+
+def substrings(a, b, n):
+    """Return substrings of length n in both a and b"""
+
+    a_substr = set()
+    for i in range(0, len(a) - n):
+        a_substr.add(a[i:i + n])
+
+    return set([ss for ss in a_substr if ss in b])
